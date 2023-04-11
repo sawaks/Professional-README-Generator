f17343ea3e6b22c9e3b4e062b55464b7a9aa7816
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-04-11 13:37:26 +1000

    Coded some functions about license at generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,56 +1,87 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (license) {
+    const badges = {
+      mit: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+      isc: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+      gnugplv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    }
+    return badges[license];
+  } else {
+    return '';
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license) {
+    const link = {
+      mit: '[MIT](https://choosealicense.com/licenses/mit/)',
+      isc: '[ISC](https://choosealicense.com/licenses/isc/)',
+      gnugplv3: '[GNUGPLv3](https://choosealicense.com/licenses/gpl-3.0/)'
+    }
+    return link[license];
+  } else {
+    return '';
+  }
 
+
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license) {
+    return `Licensed belong to the ${renderLicenseLink(license)} license`
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
   return `
-  # ${data.title}
+# ${data.title}
+${renderLicenseBadge(data.license)}
 
-  ## Description
-  ${data.description}
+## Description
+${data.description}
 
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [License](#license)
-  - [GitHub Profile](#username)
-  - [Email](#email)
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [GitHub Profile](#github-profile)
+- [Email](#email)
 
-  ## Installation
-  ${data.installation}
+## Installation
+${data.installation}
 
-  ## Usage
-  ${data.usage}
+## Usage
+${data.usage}
 
-  ## Credits
-  ${data.credits}
+## Credits
+${data.credits}
 
-  ## Contributing
-  ${data.contributing}
+## Contributing
+${data.contributing}
 
-  ## Tests
-  ${data.tests}
+## Tests
+${data.tests}
 
-  ## License
-  ${data.license}
+## License
+${renderLicenseSection(data.license)}
 
-  ## GitHub Profile
-  https://github.com/ ${data.username}
+## GitHub Profile
+https://github.com/ ${data.username}
   
-  ## Email
-  ${data.email}
+## Email
+${data.email}
 
  `;
 }
